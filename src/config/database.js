bbd213fc6d6f985c7db8c0fcfadc4c71032ca2bb
Amel <ameladdou213@gmail.com>
2023-05-26 16:44:28 +0200

    data base configurée

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mysql://amel:mdp@localhost:3306/cookolisto')
+const sequelize = new Sequelize("cookolisto-db",'mysql', '6d1bd742a95e7447', {
+    host:"dokku-mysql-cookolisto-db",
+    port:3306,
+    dialect: "mysql",
+    //logging: false
+  });
 
 module.exports = sequelize;
