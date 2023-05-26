fe0adba7b1a61a313341d3f05882fc9314d9cf9d
Amel <ameladdou213@gmail.com>
2023-05-26 16:57:42 +0200

    database url déja crée par dokku

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("cookolisto-db",'mysql', '6d1bd742a95e7447', {
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+/*const sequelize = new Sequelize("cookolisto-db","mysql", "6d1bd742a95e744", {
     host:"dokku-mysql-cookolisto-db",
     port:3306,
     dialect: "mysql",
     //logging: false
-  });
+  });*/
 
 module.exports = sequelize;
