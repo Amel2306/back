dbe9cf6970da3f2a4480a215b7610abc846d46c3
Amel <ameladdou213@gmail.com>
2023-05-26 16:13:33 +0200

    ajout de config dans git ignore

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MDP, {
-    dialect: 'mysql',
-  });
+const sequelize = new Sequelize('mysql://amel:mdp@localhost:3306/cookolisto')
 
 module.exports = sequelize;
