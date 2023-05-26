6b4c4cae069eeb8992d5d9544f3f6b381a9617cf
Amel <ameladdou213@gmail.com>
2023-05-26 16:32:44 +0200

    data base déployée

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mysql://amel:mdp@localhost:3306/cookolisto')
+const sequelize = new Sequelize('mysql://mysql:0d7af7dad2d19284@dokku-mysql-cooko-db:3306/cooko_d')
 
 module.exports = sequelize;
