0b3344e66506b05f44b3fc81daa6dd787e733828
Amel <ameladdou213@gmail.com>
2023-05-26 18:45:14 +0200

    modification origine des cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:8080'
+    origin: 'https://papaya-speculoos-1d07fa.netlify.app'
   }));
 
 //pour gérer les erreurs
