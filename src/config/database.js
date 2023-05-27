332740303df582bac56d8ff3810b42c0480f9db0
Amel <ameladdou213@gmail.com>
2023-05-27 05:08:58 +0200

    modificaiton sur la data base avec db dokku

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const url_db = process.env.DATABASE_URL
+//console.log("iciiiiiiiiiiiii" + url_db);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    //host:"dokku-mysql-cookolisto-db",
+    //port:3306,
+    dialect: "mysql",
+});
 
 /*const sequelize = new Sequelize("cookolisto-db","mysql", "6d1bd742a95e744", {
     host:"dokku-mysql-cookolisto-db",
