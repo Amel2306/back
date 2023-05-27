a0e27d82214f244cad84ad8dc182211cf1402be1
Amel <ameladdou213@gmail.com>
2023-05-27 05:41:07 +0200

    enlevr l'espace

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 //app.use(cors());
 app.use(cors({
-    origin: ' http://localhost:8080',
+    origin: 'http://localhost:8080',
     optionsSuccessStatus: 200 // Permettre les réponses avec le code de statut 200
 }));
 
